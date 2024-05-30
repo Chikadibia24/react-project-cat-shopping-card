da02ab2451dceb531e8f1cc2c063aa7036f834cb
ChikaDibia <ogbuchika71@gmail.com>
2024-05-30 14:18:57 +0100

    Bug fixes

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -2,11 +2,9 @@ import { FaDollarSign } from "react-icons/fa6";
 
 const Price = () => {
   return (
-    <div className="flex items-center justify-start w-[56%] md:w-[58%] text-slate-700  font-[500] text-[25px]">
+    <div className="flex items-center justify-center w-[56%] md:w-[58%] text-slate-700  font-[500] text-[25px]">
       <FaDollarSign />
-      <p className="pt-[2px]">
-        14.99
-      </p>
+      <p className="pt-[2px]">14.99</p>
     </div>
   );
 };
