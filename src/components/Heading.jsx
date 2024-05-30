c089c196bf841cad53d2f80e6fcd1d4a9c626e31
ChikaDibia <ogbuchika71@gmail.com>
2024-05-30 14:21:49 +0100

    Bug fixes

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,7 @@
 
 const Heading = () => {
   return (
-    <div className="w-[60%]">
+    <div className="w-[70%] md:w-[60%]">
       <header className="heading-text">
         <h1 className="text-slate-700   font-medium text-[43px]">
           Soft fleece cat sweater
